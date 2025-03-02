93938bb346a786f6191252da9aa1ab3794e3249f
KyleE <egank8652@gmail.com>
2025-03-02 14:41:22 -0500

    Initial commit

---
+++ src/main/java/engg1420group2/universitymanagementsystem/studentmanagement/HelloController.java
@@ -0,0 +1,14 @@
+package engg1420group2.universitymanagementsystem.studentmanagement;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module engg1420group2.universitymanagementsystem.studentmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens engg1420group2.universitymanagementsystem.studentmanagement to javafx.fxml;
+    exports engg1420group2.universitymanagementsystem.studentmanagement;
+}
