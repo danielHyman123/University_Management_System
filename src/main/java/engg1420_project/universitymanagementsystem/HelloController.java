0fa35e0175dd399c7f9cb36320fb56fe2ba95270
hyman <danielhyman20@gmail.com>
2025-03-02 23:47:11 -0500

    Inital JavaFX

---
+++ src/main/java/engg1420_project/universitymanagementsystem/HelloController.java
@@ -0,0 +1,14 @@
+package engg1420_project.universitymanagementsystem;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome our University Management System!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module engg1420_project.universitymanagementsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens engg1420_project.universitymanagementsystem to javafx.fxml;
+    exports engg1420_project.universitymanagementsystem;
+}
