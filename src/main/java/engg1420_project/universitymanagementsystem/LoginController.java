2a1267aa9361c2d52fb5665aa053bcfb7912571d
hyman <danielhyman20@gmail.com>
2025-03-06 19:03:37 -0500

    Rename HelloController to LoginController

--- src/main/java/engg1420_project/universitymanagementsystem/LoginController.java
+++ src/main/java/engg1420_project/universitymanagementsystem/LoginController.java
@@ -14,7 +14,7 @@ import javafx.stage.*;
 import java.io.IOException;
 import java.util.Objects;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText, fail;
     @FXML
